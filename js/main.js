b9af6e7df7a9aed38bc4dc03b234d22a4e9b20d0
Protorab <50131464+Protorab@users.noreply.github.com>
2020-10-12 14:32:47 +0300

    полностью готво(ага)

--- js/main.js
+++ js/main.js
@@ -36,20 +36,32 @@ $(document).ready(function () {
 
   function subMenuUp() {
     $('.sub__menu').slideUp();
-    $('.menu__drop').removeClass('__show');
+
   }
   $('.sub__menu').hover(function () {}, function () {
-    subMenuUp();
+    $('.menu__drop').removeClass('__show');
+    $('.sub__menu').slideUp();
   });
+
+
+  if (($(window).width() >= 1024)) {
+
+  }
   $('.menu__drop').on('click', function () {
     var thisSubMenu = $(this).find('.sub__menu');
     subMenuUp();
+
+    $(this).siblings('.menu__drop').removeClass('__show');
     $(this).hasClass('__show') ? $(this).removeClass('__show') : $(this).addClass('__show');
     thisSubMenu.is(':visible') ? thisSubMenu.slideUp() : thisSubMenu.slideDown();
   });
+
   $(document).keyup(function (e) {
     if (e.keyCode === 27) $('.popup__close').click(); // esc
     $('.find__form').removeClass('__show'); // esc
+    $('.burger__menu').removeClass('__active');
+    $('.menu').removeClass('__show');
+    $('.menu__bg').fadeOut();
   });
   $('.popup__img').magnificPopup({
     type: 'image',
@@ -58,9 +70,7 @@ $(document).ready(function () {
       verticalFit: false
     }
   });
-  var menuSliderImgHeigth = $('.item').width();
-  $('.menu__slider-img').height(menuSliderImgHeigth);
-  $('.eco__grid-img').height(menuSliderImgHeigth);
+
   var count = 0;
   $('.counter__plus').click(function (e) {
     var itemPrice = $(this).parents('.item').find('.menu__slider-price').text();
@@ -84,8 +94,26 @@ $(document).ready(function () {
     slidesToShow: 4,
     slidesToScroll: 1,
     dots: false,
+    infinite: true,
     nextArrow: '<div class="menu__slider-next menu__slider-arrow "><span> </span><span></span></div>',
     prevArrow: '<div class="menu__slider-prev menu__slider-arrow"><span> </span><span></span></div>',
+    responsive: [{
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
   });
   $('.reviews__slider').slick({
     slidesToShow: 1,
@@ -96,27 +124,37 @@ $(document).ready(function () {
     nextArrow: '<div class="reviews__slider-next reviews__slider-arrow "><span> </span><span></span></div>',
     prevArrow: '<div class="reviews__slider-prev reviews__slider-arrow"><span> </span><span></span></div>',
   });
-
-  $('.eco__grid-item').slice(8).slideToggle();
+  var ecoItemSlice = 0;
+  if (($(window).width() >= 800)) {
+    ecoItemSlice = 8;
+  } else {
+    ecoItemSlice = 4;
+  }
+  $('.eco__grid-item').slice(ecoItemSlice).slideToggle();
   $('.load__more').click(function (e) {
-    if ($('.eco__grid-item').slice(8).is(':visible')) {
-      $('.eco__grid-item').slice(8).slideToggle('slow');
+    if ($('.eco__grid-item').slice(ecoItemSlice).is(':visible')) {
+      $('.eco__grid-item').slice(ecoItemSlice).slideToggle('slow');
       $(this).delay(300).text('Посмотреть все');
     } else {
-      $('.eco__grid-item').slice(8).slideToggle('slow');
+      $('.eco__grid-item').slice(ecoItemSlice).slideToggle('slow');
       $(this).delay(300).text('Скрыть');
     }
     e.preventDefault();
-
   });
-  $('.burger__menu').click(function (e) {
+
+  function menuToggle() {
     $('.burger__menu').toggleClass('__active');
-    $('.m__menu').toggleClass('__show');
+    $('.menu').toggleClass('__show');
+    $('.menu__bg').is(':visible') ? $('.menu__bg').fadeOut() : $('.menu__bg').fadeIn();
+  }
+  $('.burger__menu').click(function (e) {
+menuToggle();
     e.preventDefault();
   });
   $(window).scroll(function () {
     $('.burger__menu').removeClass('__active');
-    $('.m__menu').removeClass('__show');
+    $('.menu').removeClass('__show');
+    $('.menu__bg').fadeOut();
   });
   $('.show__form').click(function (e) {
     $('.popup').hide().css("display", "flex").fadeIn();
